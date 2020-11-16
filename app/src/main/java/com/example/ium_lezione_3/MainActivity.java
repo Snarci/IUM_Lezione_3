c49962611fecfc3a8474f6b2d19674e594dd066c
Snarci <luczzedda@gmail.com>
2020-11-16 19:06:52 +0100

    Primo

---
+++ app/src/main/java/com/example/ium_lezione_3/MainActivity.java
@@ -0,0 +1,28 @@
+package com.example.ium_lezione_3;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.graphics.Color;
+import android.os.Bundle;
+
+import java.util.Arrays;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        PieChartView pieChart =this.findViewById(R.id.piechart);
+        Float[] percent = new Float[]{40.0f,20.0f,20.0f,20.0f};
+        Integer[] colors = new Integer[]{0xffedf8fb,0xffb2e2e2,0xff66c2a4,0xff66c2a4};
+        pieChart.setPercent(Arrays.asList(percent));
+        pieChart.setSegmentColor(Arrays.asList(colors));
+
+        pieChart.setRadadius(300);
+        pieChart.setStrokeColor(Color.BLACK);
+        pieChart.setStrokeWidth(4);
+    }
+}
---
+++ app/src/main/java/com/example/ium_lezione_3/PieChartView.java
@@ -0,0 +1,167 @@
+package com.example.ium_lezione_3;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.PointF;
+import android.graphics.Rect;
+import android.graphics.RectF;
+import android.util.AttributeSet;
+import android.view.View;
+
+import androidx.annotation.Nullable;
+
+import java.util.List;
+
+public class PieChartView extends View {
+    private int backgroundColor = Color.WHITE;
+    private List<Float> percent;
+    private List<Integer> segmentColor;
+    private RectF enclosing = new RectF();
+    private PointF center = new PointF();
+    private int radadius =100;
+    private int strokeColor;
+    private int strokeWidth;
+    private int selectedIndex =2;
+    private float selectedStartAngle= 0.0f;
+
+    protected void onDraw(Canvas canvas){
+        Paint paint = new Paint();
+
+        paint.setFlags(Paint.ANTI_ALIAS_FLAG);
+
+        paint.setColor(this.getBackgroundColor());
+
+        Rect rect=new Rect();
+        rect.left =0;
+        rect.right=getWidth();
+        rect.top=0;
+        rect.bottom=getHeight();
+
+        canvas.drawRect(rect,paint);
+        //percentuale e colore della singola fetta
+        float p;
+        int c;
+        center.x= canvas.getWidth() /2;
+        center.y =canvas.getHeight() /2;
+        enclosing.top=center.y-radadius;
+        enclosing.bottom=center.y+radadius;
+        enclosing.left=center.x-radadius;
+        enclosing.right=center.x+radadius;
+
+        float alpha = -90.0f;
+
+        float p2a = 360.0f/100.0f;
+        //riempie le fette
+        for(int i= 0;i < percent.size();i++){
+            p=percent.get(i);
+            c=segmentColor.get(i);
+            paint.setColor(c);
+            paint.setStyle(Paint.Style.FILL);
+            canvas.drawArc(enclosing,alpha,p*p2a,true,paint);
+            alpha+=p*p2a;
+        }
+        alpha = -90.0f;
+        for(int i= 0;i < percent.size();i++){
+            p=percent.get(i);
+            c=segmentColor.get(i);
+            paint.setColor(strokeColor);
+            paint.setStyle(Paint.Style.STROKE);
+            paint.setStrokeWidth(strokeWidth);
+            if(i==selectedIndex){
+                selectedStartAngle =alpha;
+            }
+            canvas.drawArc(enclosing,alpha,p*p2a,true,paint);
+            alpha+=p*p2a;
+        }
+    }
+
+    public PieChartView(Context context) {
+        super(context);
+    }
+
+    public PieChartView(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public PieChartView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    public int getBackgroundColor() {
+        return backgroundColor;
+    }
+
+    @Override
+    public void setBackgroundColor(int backgroundColor) {
+        this.backgroundColor = backgroundColor;
+    }
+
+    public List<Float> getPercent() {
+        return percent;
+    }
+
+    public void setPercent(List<Float> percent) {
+        this.percent = percent;
+    }
+
+    public List<Integer> getSegmentColor() {
+        return segmentColor;
+    }
+
+    public void setSegmentColor(List<Integer> segmentColor) {
+        if(segmentColor.size()!= percent.size()){
+            throw new  IllegalArgumentException("Liste con differenti dimensioni");
+        }
+        this.segmentColor = segmentColor;
+    }
+
+    public PointF getCenter() {
+        return center;
+    }
+
+    public void setCenter(PointF center) {
+        this.center = center;
+    }
+
+    public int getRadadius() {
+        return radadius;
+    }
+
+    public void setRadadius(int radadius) {
+        this.radadius = radadius;
+    }
+
+    public int getStrokeColor() {
+        return strokeColor;
+    }
+
+    public void setStrokeColor(int strokeColor) {
+        this.strokeColor = strokeColor;
+    }
+
+    public int getStrokeWidth() {
+        return strokeWidth;
+    }
+
+    public void setStrokeWidth(int strokeWidth) {
+        this.strokeWidth = strokeWidth;
+    }
+
+    public int getSelectedIndex() {
+        return selectedIndex;
+    }
+
+    public void setSelectedIndex(int selectedIndex) {
+        this.selectedIndex = selectedIndex;
+    }
+
+    public float getSelectedStartAngle() {
+        return selectedStartAngle;
+    }
+
+    public void setSelectedStartAngle(float selectedStartAngle) {
+        this.selectedStartAngle = selectedStartAngle;
+    }
+}
